cf3d8c13d1ac453293822d9bf6bdfbe6f0f3f0db
Chandra-kanth-dev <bandichandrakanth2005@gmail.com>
2025-09-04 09:24:33 +0530

    Delete basic.py

--- basic.py
+++
@@ -1,9 +0,0 @@
-#basic python program
-print("welcome to python programming")
-a='A'
-b="34"
-c=45
-print(ord(a))  #ord -->char to int
-print(int(b))  #int converts any type to integer
-print(str(c))  #string to integer
-
