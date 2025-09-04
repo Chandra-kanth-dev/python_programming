0b5394f1d1e4c588328bc8837fb983e79a94e114
Chandra-kanth-dev <bandichandrakanth2005@gmail.com>
2025-09-04 09:25:23 +0530

    Delete cylinder_props.py

--- cylinder_props.py
+++
@@ -1,6 +0,0 @@
-#CYLINDER AREA
-height = int(input("enter height of cylinder"))
-radius = int(input("enter radius of cylinder"))
-phi = 3.14
-volume = height*(phi*radius*radius)
-print("volume of cylinder is " , volume)
