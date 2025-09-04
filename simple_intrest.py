605355661c5846a540a871f6bcccc56cd11b2410
Chandra-kanth-dev <bandichandrakanth2005@gmail.com>
2025-09-04 09:26:33 +0530

    Delete simple_intrest.py

--- simple_intrest.py
+++
@@ -1,7 +0,0 @@
-#CALCULATING SIMPLE INTREST AND TOTAL AMOUNT
-principal_amt = int(input("enter principal amount"))
-time = int(input("enter time in months"))
-rate = int(input("enter the rate "))
-s_i = (principal_amt*time*rate)//100
-print("simple intrest is ", s_i)
-print("total amount is ", principal_amt+s_i)
