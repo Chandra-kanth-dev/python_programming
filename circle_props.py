043e3c4ce672c32b4130d061583ab7151eedcef9
Chandra-kanth-dev <bandichandrakanth2005@gmail.com>
2025-09-04 09:24:46 +0530

    Delete circle_props.py

--- circle_props.py
+++
@@ -1,7 +0,0 @@
-
-#finding area  and circumfurence of a cirlce
-radius=int(input("enetr radius :"))
-phi = 3.14
-
-print("the area of circle whose radius  is",radius ,"is",phi*radius*radius)
-print("the cicumfurence of the circle is", 2*phi*radius)
